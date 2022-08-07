30fb4b95bbe43e69cd1d937110f5a9a668dedc5a
Flávia Bulad <flavia.bulad@gmail.com>
2022-08-07 16:20:10 -0300

    feat: url, user and ranking routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 
 import authRouter from "./authRouter.js";
+import rankingRouter from "./rankingRouter.js";
+import urlRouter from "./urlRouter.js";
+import userRouter from "./userRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
+router.use(urlRouter);
+router.use(userRouter);
+router.use(rankingRouter);
 
 export default router;
